9b1cf94853cb48fec2f3eb11a9f9a5cecf621c22
Abid F <r.rumah112@gmail.com>
2020-12-20 06:50:45 +0700

    adding queue solution using interface, yey!

--- Problem 2 - Queue/using interface/queue.go
+++ Problem 2 - Queue/using interface/queue.go
@@ -1,36 +1,50 @@
 package queue
 
 type Queue interface {
-	Push(key interface{
-		queue = append(queue, key)
-	})
-
-	Pop() interface{
-		x = queue[0]
-		queue = queue[1:]
-	}
-
-	Contains(key interface{
-		for i:= 0; i<size; i++ {
-			if queue[i] == key {
-				return false
-			}
-		}
-		return true
-	}) bool
-
-	Len() int {
-		return size
-	}
-
-	Keys() []interface{
-		for i := 0; i<size; i++ {
-			fmt.Print(queue[i],)
-		}
-	}
+	Push(key interface{})
+	Pop() interface{}
+	Contains(key interface{}) bool
+	Len() int
+	Keys() []interface{}
 }
 
-func New(size int) Queue {
-	queue := make([]int, size)
-	return queue
+type QQ struct {
+	data []interface{}
+	size int
+}
+
+func New(size int) QQ {
+	q := QQ{make([]interface{}, 0), size}
+	return q
+}
+
+func (q *QQ) Push(key interface{}) {
+	// if len(q.data) < q.size {
+	// 	q.data = append(q.data, key)
+	// }
+
+	q.data = append(q.data, key)
+}
+
+func (q *QQ) Pop() interface{} {
+	x := q.data[0]
+	q.data = q.data[1:]
+	return x
+}
+
+func (q *QQ) Keys() []interface{} {
+	return q.data
+}
+
+func (q *QQ) Contains(key interface{}) bool {
+	for _, v := range q.data {
+		if v == key {
+			return true
+		}
+	}
+	return false
+}
+
+func (q *QQ) Len() int {
+	return len(q.data)
 }
